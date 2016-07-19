a8e9492052d3d38d840b8dd050b0163383e6dc76
Geng <snggeng@gmailcom>
2016-07-19 14:25:18 +0800

    edited user_auth.js

--- javascript/user_auth.js
+++ javascript/user_auth.js
@@ -1,10 +1,10 @@
 /* global $ pageReady */
-var serverURL = 'http://localhost:3000/'
+var serverURL = 'http://startups-sg.herokuapp.com/'
 var currentUser = null
 
 // We are assuming Local Storage is supported
 $(function () {
-  if (!window.localStorage['email'] || !window.localStorage['auth_token']) window.location.href = 'user.html'
+  if (!window.localStorage['email'] || !window.localStorage['auth_token']) window.location.href = 'login.html'
   else loadUser()
 })
 
@@ -12,7 +12,7 @@ $(function () {
 function loadUser () {
   $.ajax({
     type: 'GET',
-    url: 'https://startups-sg.herokuapp.com/user',
+    url: serverURL + 'users/' + window.localStorage['id'],
     beforeSend: function (xhr) {
       xhr.setRequestHeader('User-Email', window.localStorage['email'])
       xhr.setRequestHeader('Auth-Token', window.localStorage['auth_token'])
@@ -26,7 +26,9 @@ function loadUser () {
     },
     error: function (xhr, ajaxOptions, thrownError) {
       // else error, redirect to login
-      window.location.href = 'user.html'
+      window.location.href = 'login.html'
     }
   })
+  if (currentUser) { $('#login').hide() }
+  if (currentUser) { $('#signup').hide() }
 }
